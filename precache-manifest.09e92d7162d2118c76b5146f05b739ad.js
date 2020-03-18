c661067b14d314d03c3da05043c435ce1f09d6b5
unknown <kritsadamoungkhao@gmail.com>
2020-03-19 00:02:20 +0700

    Updates

---
+++ precache-manifest.09e92d7162d2118c76b5146f05b739ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef736c54e6f0647a5bd55420dc8453a0",
+    "url": "/kidkrub/index.html"
+  },
+  {
+    "revision": "97116987fec8291170ba",
+    "url": "/kidkrub/static/css/main.4d6f634b.chunk.css"
+  },
+  {
+    "revision": "e1a31469ad81c872ba8b",
+    "url": "/kidkrub/static/js/2.e125e370.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kidkrub/static/js/2.e125e370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97116987fec8291170ba",
+    "url": "/kidkrub/static/js/main.8576126f.chunk.js"
+  },
+  {
+    "revision": "e415b00822bf275578bf",
+    "url": "/kidkrub/static/js/runtime-main.03f27fd5.js"
+  }
+]);
